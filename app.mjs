96a403d35133442cfdf4ca984d6ba13f6efb94a2
@maneesha.gunawardhana <maneesha.gunawardhana@zincat.net>
2024-08-08 09:54:15 +0530

    content update

--- app.mjs
+++ app.mjs
@@ -8,4 +8,22 @@ const accountName = process.env.ACCOUNT_NAME;
 const sasToken = process.env.SAS_TOKEN;
 const containerName = process.env.CONTAINER_NAME;
 
-const BlobServiceClient = new BlobServiceClient(`https://${accountName}.blob.core.windows.net${sasToken}`);
+const blobServiceClient = new BlobServiceClient(`https://${accountName}.blob.core.windows.net${sasToken}`);
+
+const containerClient = blobServiceClient.getContainerClass(containerName);
+
+const client = new MongoClient(mongodbUri);
+client.connect();
+
+const server = http.createServer(handleImageUpload);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
+
+async function handleImageUpload(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+}
